e729bb585672bec616bf7fb2c372d8432f45a368
Mnyomi2 <bayrout2k1x2@outlook.com>
2025-08-05 00:40:23 +0000

    Add files via upload

---
+++ AllInOne/dart/manga/multisrc/mangabox/src/en/mangakakalot/mangakakalot.dart
@@ -0,0 +1,14 @@
+import '../../../../../../../model/source.dart';
+
+Source get mangakakalotSource => _mangakakalotSource;
+
+Source _mangakakalotSource = Source(
+  name: "Mangakakalot",
+  baseUrl: "https://www.mangakakalot.gg",
+  lang: "en",
+  typeSource: "mangabox",
+  iconUrl:
+      "https://raw.githubusercontent.com/Mnyomi2/Mnyomi2/refs/heads/main/AllInOne/dart/manga/multisrc/mangabox/src/en/mangakakalot/icon.png",
+  dateFormat: "MMM dd,yyyy",
+  dateFormatLocale: "en",
+);
---
+++ AllInOne/dart/manga/multisrc/mangabox/src/en/manganato/manganato.dart
@@ -0,0 +1,14 @@
+import '../../../../../../../model/source.dart';
+
+Source get manganatoSource => _manganatoSource;
+
+Source _manganatoSource = Source(
+  name: "Manganato",
+  baseUrl: "https://www.natomanga.com",
+  lang: "en",
+  typeSource: "mangabox",
+  iconUrl:
+      "https://raw.githubusercontent.com/Mnyomi2/Mnyomi2/refs/heads/main/AllInOne/dart/manga/multisrc/mangabox/src/en/manganato/icon.png",
+  dateFormat: "MMM dd,yy",
+  dateFormatLocale: "en",
+);
