28b98a1054309a18176ceb884426fe1c58666357
Mnyomi2 <bayrout2k1x2@outlook.com>
2025-08-11 02:16:02 +0000

    Update akwam.js

--- Mnyomi2/anime/src/ar/akwam.js
+++ Mnyomi2/anime/src/ar/akwam.js
@@ -6,7 +6,7 @@ const mangayomiSources = [{
     "iconUrl": "https://www.google.com/s2/favicons?sz=256&domain=ak.sv",
     "typeSource": "single",
     "itemType": 1,
-    "version": "1.0.7",
+    "version": "1.0.8",
     "pkgPath": "anime/src/ar/akwam.js"
 }];
 
@@ -63,10 +63,23 @@ class DefaultExtension extends MProvider {
         const hasNextPage = !!doc.selectFirst("ul.pagination li.page-item a[rel=next]");
         return { list, hasNextPage };
     }
-
+	
+	
     async getLatestUpdates(page) {
-        throw new Error("Not supported");
+        const doc = await this.requestDoc(`/recent?page=${page}`);
+        const list = [];
+        const items = doc.select("div.entry-box-1 div.entry-image a.box");
+
+        for (const item of items) {
+            list.push(this.parseAnimeFromElement(item));
+        }
+
+        return { list, hasNextPage: false };
     }
+	
+//      const hasNextPage = !!doc.selectFirst("ul.pagination li.page-item a[rel=next]");
+//      return { list, hasNextPage };
+
 
     async search(query, page, filters) {
         function getSelectValue(filter) {
